21aa93fe5c1e0a0a5ef0bca6acccd6fb68b90a96
KassieZuritaG <57915876+KassieZuritaG@users.noreply.github.com>
2021-09-17 03:46:36 -0500

    retroceso

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
 };
-export const apiUrl = 'https://shop-druggelp.herokuapp.com/api';
+export const apiUrl = 'https://shop-druggelp.herokuapp.com';
+
+//export const apiUrl = 'https://shop-druggelp.herokuapp.com/';
+//export const apiUrl = 'https://shop-druggelp.herokuapp.com/api';
+//export const apiUrl = '/api';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,16 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+/*export const environment = {
   production: false
 };
-export const apiUrl = '//localhost:8080/api';
+export const apiUrl = '//localhost:8080/api';*/
+
+
+export const environment = {
+  production: true,
+};
+export const apiUrl = 'https://shop-druggelp.herokuapp.com';
 
 /*
  * For easier debugging in development mode, you can import the following file
