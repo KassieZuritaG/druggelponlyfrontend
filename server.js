355d054fd8b8d5116b0ed68ad6bec3006f986209
KassieZuritaG <57915876+KassieZuritaG@users.noreply.github.com>
2021-09-17 01:30:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.use('https://shop-druggelp.herokuapp.com/');
+
+/*app.get('/*', function(req,res) {  
+    res.sendFile(path.join('https://shop-druggelp.herokuapp.com/'+__dirname));   
+}); 
 
 app.get('/*', function(req,res) {  
     res.sendFile(path.join('https://shop-druggelp.herokuapp.com/'+__dirname));   
-});  
+}); 
 
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
